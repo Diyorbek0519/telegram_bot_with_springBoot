b6573e61f6eb34ab21d6912b6df7e37aecbe0e96
Diyorbek Hamidov <diyorbekhamidov0519@gmail.com>
2023-09-28 15:50:36 +0500

    init commit

---
+++ src/test/java/com/example/telegram_bot_with_springCore/TelegramBotWithSpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegram_bot_with_springCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotWithSpringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
